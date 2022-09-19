13786351a2d8dc8f4afe42a8a390c52013724b0e
Md Rejwanul Hoque <bulbul.rej@gmail.com>
2022-09-19 15:40:39 +0600

    mad management link issue and order and order set design change

--- ui/app/common/patient-search/controllers/patientsListController.js
+++ ui/app/common/patient-search/controllers/patientsListController.js
@@ -178,10 +178,10 @@ angular.module('bahmni.common.patientSearch')
             if ($scope.search.searchType.links) {
                 link = _.find($scope.search.searchType.links, {linkColumn: heading}) || link;
             }
-            link.url = "#/default/patient/{{patientUuid}}/dashboard/concept-set-group/observations?encounterUuid=active";
-            if (link.url && link.url !== null) {
-                $window.open(appService.getAppDescriptor().formatUrl(link.url, options, true), link.newTab ? "_blank" : "_self");
+            if(link.url ===  "#/default/patient/{{patientUuid}}/dashboard?encounterUuid=active"){
+                link.url = "#/default/patient/{{patientUuid}}/dashboard/concept-set-group/observations?encounterUuid=active";
             }
+            $window.open(appService.getAppDescriptor().formatUrl(link.url, options, true), link.newTab ? "_blank" : "_self");
         };
         var getPatientCountSeriallyBySearchIndex = function (index) {
             if (index === $scope.search.searchTypes.length) {
