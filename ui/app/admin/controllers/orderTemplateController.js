35130c31cc125300044f61ac17873332e0f636c3
Sadaf Rahman <62661608+rahmansadaf46@users.noreply.github.com>
2022-11-28 11:15:41 +0600

    oreder template bug fixing

--- ui/app/admin/controllers/orderTemplateController.js
+++ ui/app/admin/controllers/orderTemplateController.js
@@ -6,7 +6,7 @@
             'drug': {
                 'name': drug.name,
                 'uuid': drug.uuid,
-                'form': drug.dosageForm?.display
+                'form': drug.dosageForm ? drug.dosageForm.display : ''
             },
             'value': drug.name
         };
