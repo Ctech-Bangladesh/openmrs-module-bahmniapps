840a2f12b3ece19f91225cb3024b495cc51d7d80
sadathossain-web <shimpux3@gmail.com>
2025-09-17 14:33:20 +0600

    code updated

--- ui/app/common/patient-search/controllers/patientsListController.js
+++ ui/app/common/patient-search/controllers/patientsListController.js
@@ -182,14 +182,7 @@ angular.module('bahmni.common.patientSearch')
                 }
 
                 var redirect = function () {
-                    if (link.url && link.url !== null) {
-                        if (link.url === "#/default/patient/{{patientUuid}}/dashboard?encounterUuid=active") {
-                            $window.location.href = (`https://${$window.location.hostname}:5555/public/to-patient-clinical-dashboard/${patient.uuid}`);
-                        }
-                        else {
-                            $window.open(appService.getAppDescriptor().formatUrl(link.url, options, true), link.newTab ? "_blank" : "_self");
-                        }
-                    }
+                    $window.open(appService.getAppDescriptor().formatUrl(link.url, options, true), link.newTab ? "_blank" : "_self");
                 };
 
                 fetch(`https://${$window.location.hostname}/openmrs/ws/rest/v1/patient/${patient.uuid}?v=full`)
